333678b1ab550a5d3ae85cde189d3e6f9cea9185
vocalvista2012-arch <vocalvista2012@gmail.com>
2026-07-14 19:37:47 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
+
+// Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDgyxk97l7CTsmgGADDvJ8jhiN6Df7WRwQ",
   authDomain: "vocalvista-fb898.firebaseapp.com",
@@ -10,29 +12,62 @@ const firebaseConfig = {
   appId: "1:89615036410:web:e50666f37f7416cbc8be43"
 };
 
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
+
+// Login Button
 const loginBtn = document.getElementById("loginBtn");
+
 
 loginBtn.addEventListener("click", () => {
 
-const email = document.getElementById("email").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
 
-const password = document.getElementById("password").value;
 
-signInWithEmailAndPassword(auth,email,password)
+    if(email === "" || password === ""){
+        document.getElementById("message").innerHTML =
+        "⚠️ Please enter email and password";
+        return;
+    }
 
-.then((userCredential)=>{
 
-document.getElementById("message").innerHTML="✅ Login Successful!";
+    signInWithEmailAndPassword(auth, email, password)
 
-setTimeout(()=>{
+    .then((userCredential)=>{
 
-window.location.href="dashboard.html";
+        const user = userCredential.user;
 
-},1000);
+        console.log("Login User:", user.email);
 
-})
-document.getElementById("message").innerHTML =
-    error.code + "<br>" + error.message;
+
+        document.getElementById("message").innerHTML =
+        "✅ Login Successful!";
+
+
+        setTimeout(()=>{
+
+            window.location.href = "dashboard.html";
+
+        },1000);
+
+
+    })
+
+
+    .catch((error)=>{
+
+        console.log(error.code);
+        console.log(error.message);
+
+
+        document.getElementById("message").innerHTML =
+        "❌ " + error.code + "<br>" + error.message;
+
+    });
+
+
+});

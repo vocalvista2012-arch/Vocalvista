1ae8a2b00fa7591c5ba89257300837f47c392352
vocalvista2012-arch <vocalvista2012@gmail.com>
2026-07-14 18:54:06 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgyxk97l7CTsmgGADDvJ8jhiN6Df7WRwQ",
+  authDomain: "vocalvista-fb898.firebaseapp.com",
+  projectId: "vocalvista-fb898",
+  storageBucket: "vocalvista-fb898.firebasestorage.app",
+  messagingSenderId: "89615036410",
+  appId: "1:89615036410:web:e50666f37f7416cbc8be43"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", () => {
+
+const email = document.getElementById("email").value.trim();
+
+const password = document.getElementById("password").value;
+
+signInWithEmailAndPassword(auth,email,password)
+
+.then((userCredential)=>{
+
+document.getElementById("message").innerHTML="✅ Login Successful!";
+
+setTimeout(()=>{
+
+window.location.href="dashboard.html";
+
+},1000);
+
+})
+
+.catch((error)=>{
+
+document.getElementById("message").innerHTML=error.message;
+
+});
+
+});

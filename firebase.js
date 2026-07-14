94665b905fbb3ca1a2a71808b87c4bc81431c38e
vocalvista2012-arch <vocalvista2012@gmail.com>
2026-07-14 16:43:36 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgyxk97l7CTsmgGADDvJ8jhiN6Df7WRwQ",
+  authDomain: "vocalvista-fb898.firebaseapp.com",
+  projectId: "vocalvista-fb898",
+  storageBucket: "vocalvista-fb898.firebasestorage.app",
+  messagingSenderId: "89615036410",
+  appId: "1:89615036410:web:e50666f37f7416cbc8be43",
+  measurementId: "G-W4CTRCFX44"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
